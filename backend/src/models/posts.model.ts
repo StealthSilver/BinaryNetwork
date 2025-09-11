95a5b14251c4345d05c835d660daf0a48b1a2337
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-11 23:15:31 +0530

    post model

---
+++ backend/src/models/posts.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IPost extends Document {
+  userId: mongoose.Types.ObjectId;
+  body: string;
+  likes: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PostSchema: Schema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    media: {
+      type: String,
+      default: "",
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    fileType: {
+      type: String,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model<IPost>("Post", PostSchema);
