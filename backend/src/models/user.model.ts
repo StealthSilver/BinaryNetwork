31b3b9fb117ac6a0f51f75c73fb906bc13a103ae
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-12 13:19:08 +0530

    post model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,39 +1,48 @@
-import mongoose, { Schema } from "mongoose";
-import { activeCheck } from "../controllers/posts.controller";
+import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "default.jpg",
+    },
+    token: {
+      type: String,
+      default: "",
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-    default: "default.jpg",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  token: {
-    type: String,
-    default: "",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
