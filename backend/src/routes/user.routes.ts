8ce48633def7cb5c389eff33cde519eeabc82d1e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-18 11:44:29 +0530

    register route complete

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,7 +1,17 @@
 import { Router } from "express";
 import { register, login } from "../controllers/user.controller";
+import multer from "multer";
 
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
 router.post("/register", register);
 router.post("/login", login);
