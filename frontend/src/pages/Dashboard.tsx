031b844706dde50fcce5d3c2c0b28914fef8f6c9
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-21 22:39:37 +0530

    store

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useReducer } from "react";
+
 export default function Dashboard() {
+  const router = useRuter();
+
+  useEffect(() => {
+    if (localStorage.getItem("token") === null) {
+      router.push("/login");
+    }
+  });
+
+  useEffect(() => {}, [isTokenThere]);
   return <div>Dashboard</div>;
 }
