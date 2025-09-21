f5a2873864734e1f8bf277869f20719553475580
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-21 12:14:36 +0530

    frontend init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import { Provider } from "react-redux";
 import { store } from "../src/config/redux/store";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import "./styles/App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import "./styles/App.css";
 
 export default function App() {
   return (
