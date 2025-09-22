bf164132eff56ce032518bf4a792ec1275057020
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-23 00:25:19 +0530

    docker

--- frontend/src/config/index.tsx
+++ frontend/src/config/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientServer = axios.create({
-  baseURL: "https://binarynetwork-1-i2z7.onrender.com/",
+  baseURL: "https://binarynetwork-1-i2z7.onrender.com/api",
 });
 
 clientServer.interceptors.request.use((config) => {
